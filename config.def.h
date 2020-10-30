92a535e040f24bd1d56226791ea78cb950554802
Phan Trung Son <phantrungson17@gmail.com>
2020-10-30 16:11:17 +0700

    e

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:pixelsize=15:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=15:antialias=true:autohint=true";
 static char *font2[] = {
 	"JoyPixels:pixelsize=15:antialias=true:autohint=true",
         "Symbola:pixelsize=15:antialias=true:autohint=true",
