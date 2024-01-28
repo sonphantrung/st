af199bf093bef0aa9db1c71bcd8f244ed560e7fd
Son Phan Trung <phantrungson17@gmail.com>
2024-01-28 18:14:05 +0700

    fix the color being weird

--- config.def.h
+++ config.def.h
@@ -157,8 +157,8 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
+unsigned int defaultfg = 259;
+unsigned int defaultbg = 258;
 unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 unsigned int background = 258;
